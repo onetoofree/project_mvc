b3f5a414bcd77d33fe7799eaed8569e030a19f26
Phil Edwards <peds@Phils-MacBook-Pro.local>
2018-09-07 12:10:52 +0100

    reg file tidy

--- view/userRegistration.php
+++ view/userRegistration.php
@@ -1,12 +1,7 @@
 <?php 
-
 require '../model/db_connect.php';
 session_start();
 
-// if(isset($_POST['register']))
-// 	{
-// 		require '../controller/registration.php';
-// 	}
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	if(isset($_POST['register']))
@@ -14,37 +9,31 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 		require '../controller/registration.php';
 	}
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Registration Form</title>
-	<script src="http://code.jquery.com/jquery-latest.min.js" charset="utf-8"></script>
-	<?php include 'css/css.html'; ?>
-</head>
+	<head>
+		<title>Registration Form</title>
+		<script src="http://code.jquery.com/jquery-latest.min.js" charset="utf-8"></script>
+		<?php include 'css/css.html'; ?>
+	</head>
+	<body>
+		<div class="form">
+		<h1>User Registration Page</h1>
 
-
-<body>
-<div class="form">
-<h1>User Registration Page</h1>
-
-<form action="userRegistration.php" method="post">
-<div class="field-wrap">
-Username<input type="text" placeholder = "Username *" name="username">
-</div>
-<div class="field-wrap">
-Email Address<input type="text" placeholder = "Email Address *" name="email">
-</div>
-<div class="field-wrap">
-Password<input type="password" placeholder = "Password *" name="password">
-</div>
-<button type="submit" class="button button-block" name="register" />Register</button>
-</form>
-
-
-</div>
-</body>
+			<form action="userRegistration.php" method="post">
+				<div class="field-wrap">
+					Username<input type="text" placeholder = "Username *" name="username">
+				</div>
+				<div class="field-wrap">
+					Email Address<input type="text" placeholder = "Email Address *" name="email">
+				</div>
+				<div class="field-wrap">
+					Password<input type="password" placeholder = "Password *" name="password">
+				</div>
+				<button type="submit" class="button button-block" name="register" />Register</button>
+			</form>
+		</div>
+	</body>
 </html>
